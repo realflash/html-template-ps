f8055e1467fa194de426e1ec794dec4cbd2bbf46
realflash2 <realflash2@60f25ad5-ef42-bdeb-fb0c-bd16c7393de6>
2011-07-04 10:17:11 +0000

    Created tag v0.1.

--- build/build.sh
+++ build/build.sh
@@ -1,40 +1,19 @@
 #!/bin/bash
 DIR="HTMLTemplate"
 NAME="html-template-ps"
-VERSION="0.1.1"
-OUT="$NAME-v$VERSION.zip"
+VERSION="0.1"
 
-function make
+if [[ ! -d $DIR ]]; then
 {
-	if [[ ! -d $DIR ]]; then
-	{
-		mkdir $DIR
-	}
-	else
-	{
-		rm -rf $DIR
-	}
-	fi
-
-	cp -r ../source $DIR
-	cp -r ../aux/* $DIR
-
-	zip -9 $OUT $DIR
-
-}
-
-function clean
-{
-	rm -rf $DIR
-	rm -f $OUT
-}
-
-if [[ "$1" == "clean" || "$1" == "CLEAN" ]]; then
-{
-	clean
+	mkdir $DIR
 }
 else
 {
-	make
+	rm -rf $DIR
 }
 fi
+
+cp -r ../source $DIR
+cp -r ../aux/* $DIR
+
+zip -9 $NAME-v$VERSION.zip $DIR
